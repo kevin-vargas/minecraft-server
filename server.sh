f253d13e8706b5a258b356177b8040390fa29aff
kevin-vargas <kevinvargasnavia@gmail.com>
2022-05-17 21:34:46 -0300

    add default flags

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JAR_FILE_NAME=server.jar
-
+DEFAULT_FLAGS="-Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 move_to_directory(){
     local DIRECTORY=build
     mkdir -p $DIRECTORY &&
@@ -35,7 +35,7 @@ add_optionals(){
 }
 
 exec_server(){
-  exec java -server ${OPTS} -jar ${JAR_FILE_NAME} nogui
+  exec java -server ${DEFAULT_FLAGS} ${OPTS} -jar ${JAR_FILE_NAME} nogui
 }
 
 {
