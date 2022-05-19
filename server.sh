2186e86a6eda609ddffc13ab35a0be75fbfbe819
kevin-vargas <kevinvargasnavia@gmail.com>
2022-05-19 20:40:18 -0300

    update script

--- server.sh
+++ server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 VERSION=latest
 JAR_FILE_NAME=server.jar
-DEFAULT_FLAGS="-Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+DEFAULT_FLAGS="-Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+
 move_to_directory(){
     local DIRECTORY=build
     mkdir -p $DIRECTORY &&
@@ -24,7 +25,7 @@ replace(){
   local FILE=$3
   local FROM=$1
   local TO=$2
-  sed -i "" -e "s/${FROM}/${TO}/g" $FILE
+  sed -i "s/${FROM}/${TO}/g" $FILE
 }
 
 accept_eula(){
@@ -34,6 +35,7 @@ accept_eula(){
     wait
     replace "false" "true" eula.txt
     replace "online-mode=true" "online-mode=false" server.properties
+    return 0
   fi
 }
 
